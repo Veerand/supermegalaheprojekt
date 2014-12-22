af41bd96811cd74fdca3068bbd597ea8192a88a5
Veerand <s.neljandik@gmail.com>
2014-12-22 17:58:36 +0200

    Freeedom!!11!!!

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -45,7 +45,7 @@ angular.module('Eggly',[
         $scope.newBookmark={
             title: '',
             url: '',
-            category: $scope.currentCategory
+            category: $scope.currentCategory.name
         }
    }
 
